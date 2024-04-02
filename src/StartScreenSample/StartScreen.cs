daf6abdfe39c30e076bdfcc98912fa0be4e36715
devdevdeveau <noreply@devdevdeveau.net>
2024-04-02 11:03:35 -0400

    minimal-repro

---
+++ src/StartScreenSample/StartScreen.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StartScreenSample
+{
+    public partial class StartScreen : Form
+    {
+        public StartScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void StartScreen_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ src/StartScreenSample/frmLvl1.cs
@@ -0,0 +1,44 @@
+﻿/*
+
+    IM NOT SURE WHAT TO LEAVE OUT IN THIS FILE
+
+*/
+
+using System.Media;
+
+namespace StartScreenSample
+{
+    public partial class frmLvl1 : Form
+    {
+        public frmLvl1()
+        {
+            InitializeComponent();
+            InitializeSound();
+        }
+
+        List<PictureBox> explosions;
+
+        private List<PictureBox> lasers;
+
+        private SoundPlayer music = new SoundPlayer();
+        private SoundPlayer count = new SoundPlayer();
+        private SoundPlayer go = new SoundPlayer();
+        private SoundPlayer gameOver = new SoundPlayer();
+
+        private void InitializeSound()
+        {
+            
+        }
+
+        private const int pos1 = 36, pos2 = 132, pos3 = 227, pos4 = 323, pos5 = 420, pos6 = 515, pos7 = 611, pos8 = 708, pos9 = 804;
+        private bool active = false;
+
+        private int cd = 5;
+
+        
+
+        
+
+        private int currentPos = 5;
+    }
+}
